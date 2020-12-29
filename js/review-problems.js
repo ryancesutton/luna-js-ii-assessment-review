e8d223e5f898f062013e3419c7d4d66d95ae4509
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-12-29 12:19:43 -0600

    added problems

--- js/review-problems.js
+++ js/review-problems.js
@@ -2,10 +2,10 @@
 
 //TODO: Write a function named 'typeOfValue' that takes in a value and returns its type.
 
-//typeOfValue("hello") // "string"
-// typeOfValue(123) // "number"
-// typeOfValue([]) // "object"
-// typeOfValue([4,5,6]) // "object"
+//typeOfValue("hello")      // "string"
+// typeOfValue(123)         // "number"
+// typeOfValue([])          // "object"
+// typeOfValue([4,5,6])     // "object"
 
 
 
@@ -13,9 +13,9 @@
 
 //TODO: Write a function named 'isPositive' that takes in a number and returns true or false based on whether the input is positive.
 
-//isPositive(2) // true
-// isPositive(-4) // false
-// isPositive(0) // false
+//isPositive(2)         // true
+// isPositive(-4)       // false
+// isPositive(0)        // false
 
 
 
@@ -23,9 +23,9 @@
 
 //TODO: Write a function named getLowestNumber that takes in 3 arguments. If all 3 inputs are numbers or numeric strings, then return the lowest number. If any of the 3 inputs is missing or non-numeric, then return false.
 
-// getLowestNumber(1, 3, 2)           // 1
-// geLowestNumber(0, 1, "2")         // 0
-//getLowestNumber(1, 2, 'x')         // false
+// getLowestNumber(1, 3, 2)         // 1
+// geLowestNumber(0, 1, "2")        // 0
+//getLowestNumber(1, 2, 'x')        // false
 
 
 
@@ -33,9 +33,9 @@
 
 //TODO: Write a function named subtract that takes in two inputs. If both inputs provided are numeric, subtract will return the difference of both inputs. If one or both inputs is not numeric, subtract should return false.
 
-//subtract(2, 1)           // 1
-//subtract("2", 4)         // -2
-//subtract(true, false)    // false
+//subtract(2, 1)             // 1
+//subtract("2", 4)           // -2
+//subtract(true, false)      // false
 
 
 
@@ -54,7 +54,7 @@
 
 //TODO: Write a function named isSumLess100 that accepts two inputs (x, y).  If one or more of the inputs is nonnumeric, return false.  Return true if the sum of both inputs is less than 100, otherwise return false.
 
-//isSumLess100(2, 49)        // true
-//isSumLess100("7", 97)      // false
-//isSumLess100("puppy", 8)   // false
-//isSumLess100("50", "49")   // true
+//isSumLess100(2, 49)           // true
+//isSumLess100("7", 97)         // false
+//isSumLess100("puppy", 8)      // false
+//isSumLess100("50", "49")      // true
