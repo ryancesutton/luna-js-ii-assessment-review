a9ee84c1c222c7839531f1a7b3219e88afb0d09a
Ry Sutton <ryancesutton@gmail.com>
2020-12-29 15:53:42 -0600

    add solutions for assessment review

--- js/review-problems.js
+++ js/review-problems.js
@@ -2,10 +2,18 @@
 
 //TODO: Write a function named 'typeOfValue' that takes in a value and returns its type.
 
-//typeOfValue("hello")      // "string"
-// typeOfValue(123)         // "number"
-// typeOfValue([])          // "object"
-// typeOfValue([4,5,6])     // "object"
+function typeOfValue(input) {
+
+    return typeof(input);
+
+}
+
+
+
+console.log(typeOfValue("hello"))      // "string"
+console.log(typeOfValue(123))       // "number"
+console.log(typeOfValue([]))          // "object"
+console.log(typeOfValue([4,5,6]))     // "object"
 
 
 
@@ -13,9 +21,19 @@
 
 //TODO: Write a function named 'isPositive' that takes in a number and returns true or false based on whether the input is positive.
 
-//isPositive(2)         // true
-// isPositive(-4)       // false
-// isPositive(0)        // false
+function isPositive(x) {
+
+    if (x > 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(isPositive(2))         // true
+console.log(isPositive(-4));       // false
+console.log(isPositive(0));        // false
 
 
 
@@ -23,9 +41,19 @@
 
 //TODO: Write a function named getLowestNumber that takes in 3 arguments. If all 3 inputs are numbers or numeric strings, then return the lowest number. If any of the 3 inputs is missing or non-numeric, then return false.
 
-// getLowestNumber(1, 3, 2)         // 1
-// geLowestNumber(0, 1, "2")        // 0
-//getLowestNumber(1, 2, 'x')        // false
+function getLowestNumber(x, y, z) {
+
+    if (isNaN(x) || isNaN(y) || isNaN(z) ) {
+        return false;
+    } else {
+        return Math.min(x, y, z);
+    }
+
+}
+
+console.log(getLowestNumber(1, 3, 2));         // 1
+console.log(getLowestNumber(0, 1, "2"));        // 0
+console.log(getLowestNumber(1, 2, 'x'));        // false
 
 
 
@@ -33,9 +61,22 @@
 
 //TODO: Write a function named subtract that takes in two inputs. If both inputs provided are numeric, subtract will return the difference of both inputs. If one or both inputs is not numeric, subtract should return false.
 
-//subtract(2, 1)             // 1
-//subtract("2", 4)           // -2
-//subtract(true, false)      // false
+
+function subtract(x, y) {
+
+    if (isNaN(parseFloat(x)) || isNaN(parseFloat(y)) ) {
+        return false;
+    } else {
+        return x - y;
+    }
+
+}
+
+console.log(subtract(2, 1));             // 1
+console.log(subtract("2", 4));          // -2
+console.log(subtract(true, false));     // false
+console.log(subtract("string", "string2"))
+console.log(subtract(null, 2))
 
 
 
@@ -43,10 +84,19 @@
 
 //TODO: Write a function named divisibleByThree that takes in an input and returns a boolean indicating whether the input is divisible by 3.  Nonnumeric inputs should return false.
 
-//divisibleByThree(3)         // true
-//divisibleByThree("6")       // true
-//divisibleByThree("8")       // false
-//divisibleByThree("red")     // false
+
+function divisibleByThree(input) {
+    if (isNaN(input) || input % 3 !== 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+console.log(divisibleByThree(3));         // true
+console.log(divisibleByThree("6"));       // true
+console.log(divisibleByThree("8"));       // false
+console.log(divisibleByThree("red"));     // false
 
 
 
@@ -54,7 +104,18 @@
 
 //TODO: Write a function named isSumLess100 that accepts two inputs (x, y).  If one or more of the inputs is nonnumeric, return false.  Return true if the sum of both inputs is less than 100, otherwise return false.
 
-//isSumLess100(2, 49)           // true
-//isSumLess100("7", 97)         // false
-//isSumLess100("puppy", 8)      // false
-//isSumLess100("50", "49")      // true
+
+function isSumLess100(x, y) {
+
+    if (parseFloat(x) + parseFloat(y) < 100) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(isSumLess100(2, 49));           // true
+console.log(isSumLess100("7", 97));         // false
+console.log(isSumLess100("puppy", 8));      // false
+console.log(isSumLess100("50", "49"));      // true
